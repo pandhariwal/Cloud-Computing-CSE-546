f35c17088b8031a7df2a6f9ef5f9b5f8ea446b4a
pandhariwal <vinay.pandhariwal@gmail.com>
2023-09-24 23:35:43 -0700

    Project upload

---
+++ Project 3/AppTier/constants.py
@@ -0,0 +1,14 @@
+class Constants:
+    AWS_ACCESS_KEY_ID = "AKIAQMYAW24ZTXBO3O4H"
+
+    AWS_SECRET_KEY = "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"
+
+    SQS_INPUT_QUEUE = "input-queue-alpha-p3.fifo"
+
+    SQS_OUTPUT_QUEUE = "output-queue-alpha-p3.fifo"
+
+    S3_INPUT_BUCKET = "input-bucket-alpha-p3"
+
+    S3_OUPUT_BUCKET = "output-bucket-alpha-p3"
+
+    AWS_REGION = "us-east-1"
